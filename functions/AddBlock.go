bbeccfb1a271a36ccbc44e66faa0573d01de3fb4
Carlo Butelli <carlo@digifit.eu>
2019-01-03 02:19:52 +0100

    first version of simple blockchain tech

--- functions/AddBlock.go
+++
@@ -1,8 +0,0 @@
-package functions
-
-// Gets pointer for blockchain
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, newBlock)
-}
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha512"
 	"fmt"
 )
 
@@ -19,6 +21,28 @@ type BlockChain struct {
 	blocks []*Block
 }
 
+// Create the hash based on the previous hash and the data
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha512.Sum512(info)
+	// Push the created hash into the block field
+	b.Hash = hash[:]
+}
+
+// Takes data and the previous hash from the last block and returns a pointer to a block
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+// Gets pointer for blockchain
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, newBlock)
+}
+
 func Alpha() *Block {
 	return CreateBlock("Alpha", []byte{})
 }
@@ -35,8 +59,7 @@ func main() {
 	chain.AddBlock("Omega Block")
 
 	for _, block := range chain.blocks {
-		// fmt.Printf("Previous hash: %x\n", block.PrevHash)
 		fmt.Printf("Data in block: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n\n", block.Hash)
+		fmt.Printf("Hash: %x\n", block.Hash)
 	}
 }
