65e783a1982d07b73406531d36a9efe4013ad63a
Carlo Butelli <carlo@digifit.eu>
2019-01-03 01:56:11 +0100

    first version of simple blockchain tech

---
+++ add-block.go
@@ -0,0 +1,8 @@
+package main
+
+// Gets pointer for blockchain
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	newBlock := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, newBlock)
+}
---
+++ create-block.go
@@ -0,0 +1,8 @@
+package main
+
+// Takes data and the previous hash from the last block and returns a pointer to a block
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
